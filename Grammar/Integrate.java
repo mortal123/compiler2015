ed638760be8532e25fdb5667b3f95a4af08fddca
mortal <liwenhao@liwenhaodeMacBook-Pro.local>
2015-04-12 17:16:04 +0800

    new

---
+++ Grammar/Integrate.java
@@ -0,0 +1,20 @@
+//package Grammar;
+
+import org.antlr.v4.runtime.*; 
+import org.antlr.v4.runtime.tree.*;
+/*
+import GrammarLexer;
+import GrammarParser;
+import ProgramContext;
+*/
+public class Integrate {
+	public static void main(String[] args) throws Exception {
+              // create a CharStream that reads from standard input
+		ANTLRInputStream input = new ANTLRInputStream(System.in); // create a lexer that feeds off of input CharStream
+		GrammarLexer lexer = new GrammarLexer(input); // create a buffer of tokens pulled from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
+		GrammarParser parser = new GrammarParser(tokens);
+		ParseTree tree = parser.program(); // begin parsing at init rule
+		System.out.println(tree.toStringTree(parser)); // print LISP-style tree }
+	}
+}
